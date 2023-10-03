98aca8bb244fdeeddb8700c7014d8ce65b2173b8
Manank Patel <manank@manank.in>
2023-10-03 23:28:23 +0530

    run script throuch shellcheck and fix warnings/issues

--- start.sh
+++ start.sh
@@ -37,40 +37,40 @@ echo ${count} > ${COUNT_FILE}
 OUTPUT_DIR="/mnt/io_uring/results/${count}-$(date)"
 
 #check redis/dragonfly
-if [ $1 = 'r' ]; then
+if [ "$1" = 'r' ]; then
     SRV="$(which redis-server)"
-elif [ $1 = 'd']; then
+elif [ "$1" = 'd' ]; then
     SRV="$(which dragonfly)"
 fi
 
 #check if record count and operation count are specified, note that both must be specified
 #or none. if not specified, chenge to default value of 1 million
-if [ [ -n RECORD_COUNT ] -a [-n OPERATION_COUNT] ]; then
-    sed -i -e "s/^recordcount=.*/recordcount=${RECORD_COUNT}/" ${WORKLOAD_DIR}/${WORKLOAD_TYPE}
-    sed -i -e "s/^operationcount=.*/operationcount=${RECORD_COUNT}/" ${WORKLOAD_DIR}/${WORKLOAD_TYPE}
+if [ $# -eq 4 ]; then
+    sed -i -e "s/^recordcount=.*/recordcount=${RECORD_COUNT}/" ${WORKLOAD_DIR}/"${WORKLOAD_TYPE}"
+    sed -i -e "s/^operationcount=.*/operationcount=${OPERATION_COUNT}/" ${WORKLOAD_DIR}/"${WORKLOAD_TYPE}"
 else
-    sed -i -e "s/^recordcount=.*/recordcount=1000000/" ${WORKLOAD_DIR}/${WORKLOAD_TYPE}
-    sed -i -e "s/^operationcount=.*/operationcount=1000000/" ${WORKLOAD_DIR}/${WORKLOAD_TYPE}
+    sed -i -e "s/^recordcount=.*/recordcount=1000000/" ${WORKLOAD_DIR}/"${WORKLOAD_TYPE}"
+    sed -i -e "s/^operationcount=.*/operationcount=1000000/" ${WORKLOAD_DIR}/"${WORKLOAD_TYPE}"
 fi
 
 #start the selected server redis/dragonfly
-${SRV} > ${OUTPUT_DIR}/${SRV}.log &
+${SRV} > "${OUTPUT_DIR}"/"${SRV}".log &
 SRV_PID=$!
 
 #run ycsb
-$YCSC_SH load redis -s -P ${YCSB_BASE}/workloads/${WORKLOAD_TYPE} -p "redis.host=${HOST}" -p "redis.port=${PORT}" >> ${OUTPUT_DIR}/ycsb-out.txt 2>> ${OUTPUT_DIR}/ycsb-err.txt
-$YCSC_SH run redis -s -P ${YCSB_BASE}/workloads/${WORKLOAD_TYPE} -p "redis.host=${HOST}" -p "redis.port=${PORT}" >> ${OUTPUT_DIR}/ycsb-out.txt 2>> ${OUTPUT_DIR}/ycsb-err.txt &
+$YCSC_SH load redis -s -P ${YCSB_BASE}/workloads/"${WORKLOAD_TYPE}" -p "redis.host=${HOST}" -p "redis.port=${PORT}" >> "${OUTPUT_DIR}"/ycsb-out.txt 2>> "${OUTPUT_DIR}"/ycsb-err.txt
+$YCSC_SH run redis -s -P ${YCSB_BASE}/workloads/"${WORKLOAD_TYPE}" -p "redis.host=${HOST}" -p "redis.port=${PORT}" >> "${OUTPUT_DIR}"/ycsb-out.txt 2>> "${OUTPUT_DIR}"/ycsb-err.txt &
 
 #wait for ycsb to setup maven
 sleep 5
 
-syscounts -L -p ${SRV_PID} >> ${OUTPUT_DIR}/syscounts-out.txt 2>> ${OUTPUT_DIR}/syscounts-err.txt &
+syscounts -L -p ${SRV_PID} >> "${OUTPUT_DIR}"/syscounts-out.txt 2>> "${OUTPUT_DIR}"/syscounts-err.txt &
 SYSCOUNT_PID=$!
 
-cachestat >> ${OUTPUT_DIR}/cachestat-out.txt 2>> ${OUTPUT_DIR}/cachestat-err.txt &
+cachestat >> "${OUTPUT_DIR}"/cachestat-out.txt 2>> "${OUTPUT_DIR}"/cachestat-err.txt &
 CACHESTAT_PID=$!
 
-sar -B >> ${OUTPUT_DIR}/sar-out.txt 2>> ${OUTPUT_DIR}/sar-err.txt $
+sar -B >> "${OUTPUT_DIR}"/sar-out.txt 2>> "${OUTPUT_DIR}"/sar-err.txt $
 SAR_PID=$!
 
 
